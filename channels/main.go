31061fd07c597790f8ba545f38b19f61bdb94767
Geunyoung Kim <kgysmart@gmail.com>
2018-10-30 10:02:34 -0700

    Add Sleeping Routine

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -20,8 +21,9 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for l := range c {
+		time.Sleep(5 * time.Second)
+		go checkLink(l, c)
 	}
 }
 
@@ -29,10 +31,10 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Yep its up"
+	c <- link
 }
